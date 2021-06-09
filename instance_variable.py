7cd865205ae3d0a628218f1f47283cd03ee04c82
kaiserkonok <kaiserkonok@gmail.com>
2021-06-09 23:03:31 +0600

    Added the course name

--- instance_variable.py
+++ instance_variable.py
@@ -1,7 +1,12 @@
 class BestCourses:
     website = 'http://www.example.com'
 
+    def __init__(self, course_name):
+        self.course_name = course_name
 
-best_course_1 = BestCourses()
+
+best_course_1 = BestCourses('Python All you Need to Know')
 
 print(best_course_1.website)
+
+print(best_course_1.course_name)
