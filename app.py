71decf7a4f7af555348206c2d03e370eb8bc626e
kaiserkonok <kaiserkonok@gmail.com>
2021-06-09 21:11:04 +0600

    Added a class in our program

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print('Hello world')
+class Rectangle:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+
+rect1 = Rectangle(100, 500)
+
+print(rect1.height)
+print(rect1.width)
