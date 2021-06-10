328575a0417b5eefb95f28cc2d31b759ed669f70
kaiserkonok <kaiserkonok@gmail.com>
2021-06-10 15:52:20 +0600

    Created our own iterator

--- iterator_tutorial.py
+++ iterator_tutorial.py
@@ -7,4 +7,28 @@ it = iter(groceries)
 print(next(it))
 print(next(it))
 print(next(it))
-print(next(it))  # StopIteration Error
+# print(next(it))  # StopIteration Error
+
+
+# let's create our own iterator
+
+class Iterator:
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+
+    def my_next(self):
+        self.index -= 1
+        if self.index < 0:
+            raise StopIteration
+        else:
+            return self.data[self.index]
+
+
+my_data = [1, 2, 3]
+
+my_iterator = Iterator(my_data)
+
+print(my_iterator.my_next())
+print(my_iterator.my_next())
+print(my_iterator.my_next())
